8eb6524f40b9e89ae7163050de1a4a26452af4df
silvestrijuanignacio@gmail.com <silvestrijuanignacio@gmail.com>
2024-11-28 20:29:02 -0300

    w02 - fix de match

--- src/Core.hs
+++ src/Core.hs
@@ -3,66 +3,54 @@ module Core (Match(..), match) where
 data Match = Correcto | LugarIncorrecto | NoPertenece
   deriving (Eq, Show)
 
-
 match :: String -> String -> [(Char, Match)]
-match objetivo intento = 
-  let statuses = []
+match intento solucion = matchesFinal
+  where
+    -- Inicializamos las listas de matches y caracteres checkeados
+    caracteresCheckeadosInit = replicate (length intento) False
+    matchesInit = replicate (length intento) NoPertenece
+
+    -- Procesamos primero los casos correctos
+    (matchesCorrectos, caracteresCorrectos) =
+      foldl checkCasosCorrectos (matchesInit, caracteresCheckeadosInit) (zip3 intento solucion [0..])
+
+    (matchesCorrectosIncorrectos, _) =
+      foldl (checkCasosIncorrectos solucion) (matchesCorrectos, caracteresCorrectos) (zip intento [0..])
+
+    matchesFinal = zip intento matchesCorrectosIncorrectos
+
+-- Maneja los casos Correcto
+checkCasosCorrectos :: ([Match], [Bool]) -> (Char, Char, Int) -> ([Match], [Bool])
+checkCasosCorrectos (matches, checkeados) (letraIntento, letraSolucion, posicion) =
+  if letraIntento == letraSolucion then
+    let checkeadosActualizados = reemplazarValor posicion True checkeados
+        matchesActualizados = reemplazarValor posicion Correcto matches
+    in (matchesActualizados, checkeadosActualizados)
+  else
+    let checkeadosActualizados = reemplazarValor posicion False checkeados
+        matchesActualizados = reemplazarValor posicion NoPertenece matches
+    in (matchesActualizados, checkeadosActualizados)
+
+-- Maneja los casos LugarIncorrecto o NoPertenece
+checkCasosIncorrectos :: String -> ([Match], [Bool]) -> (Char, Int) -> ([Match], [Bool])
+checkCasosIncorrectos solucion (matches, checkeados) (letraIntento, posicion)
+  | (matches !! posicion) == Correcto = (matches, checkeados)
+  | fst (letraEnPalabra letraIntento solucion checkeados) =
+    let checkeadosActualizados = reemplazarValor (snd (letraEnPalabra letraIntento solucion checkeados)) True checkeados
+        matchesActualizados = reemplazarValor posicion LugarIncorrecto matches
+    in (matchesActualizados, checkeadosActualizados)
+  | otherwise =
+    let checkeadosActualizados = reemplazarValor (snd (letraEnPalabra letraIntento solucion checkeados)) True checkeados
+        matchesActualizados = reemplazarValor posicion NoPertenece matches
+    in (matchesActualizados, checkeadosActualizados)
 
 
+letraEnPalabra :: Char -> String -> [Bool] -> (Bool, Int)
+letraEnPalabra letra solucion checkeados = 
+  case filter (\x -> solucion !! x == letra && not (checkeados !! x)) [0..length solucion - 1] of
+    [] -> (False, -1)
+    (posicion:_) -> (True, posicion)
 
-
-
-{-| Dada una palabra objetivo y un intento, devuelve una lista de tuplas
-    donde el primer elemento es una letra del intento y el segundo es
-    un valor de tipo Match que indica si la letra es correcta y está en
-    la posición correcta, si es correcta pero está en la posición incorrecta
-    o si no pertenece a la palabra objetivo.
-    
-    >>> match "posta" "seria"
-    [('s',LugarIncorrecto),('e',NoPertenece),('r',NoPertenece),('i',NoPertenece),('a',Correcto)]
--}
-match :: String -> String -> [(Char, Match)]
-match objetivo intento = 
-  let originalList = checkSiEsCorrecto objetivo (checkSiEsta objetivo intento)
-  in checkDuplicados objetivo intento originalList originalList
-
--- Verifica si una letra está en el objetivo en cualquier posición
-checkSiEsta :: String -> String -> [(Char, Match)] 
-checkSiEsta _ "" = []
-checkSiEsta objetivo (int:intento)
-  | int `elem` objetivo = (int, LugarIncorrecto) : checkSiEsta objetivo intento
-  | otherwise = (int, NoPertenece) : checkSiEsta objetivo intento
-
--- Marca las letras correctas (en la posición correcta)
-checkSiEsCorrecto :: String -> [(Char, Match)] -> [(Char, Match)]
-checkSiEsCorrecto _ [] = []
-checkSiEsCorrecto "" _ = []
-checkSiEsCorrecto (obj:objetivo) ((c, m):xs) = 
-  if c == obj
-    then (c, Correcto) : checkSiEsCorrecto objetivo xs
-    else (c, m) : checkSiEsCorrecto objetivo xs
-
--- Ajusta las marcas, cambiando LugarIncorrecto a NoPertenece si es necesario
-checkDuplicados :: String -> String -> [(Char, Match)] -> [(Char, Match)] -> [(Char, Match)]
-checkDuplicados _ _ _ [] = []
-checkDuplicados objetivo intento originalList lista =
-  let (c, m) = last lista
-      xs = init lista
-      countLugarInc = countLugarIncorrectoCorrecto c originalList
-      countInTarget = countChar c objetivo
-      headless_originalList = init originalList
-  in if m == LugarIncorrecto && countLugarInc > countInTarget
-     then  checkDuplicados objetivo intento headless_originalList xs ++ [(c, NoPertenece)]
-     else  checkDuplicados objetivo intento originalList xs ++ [(c, m)]
-
--- Cuenta cuántas veces aparece un carácter en un string
-countChar :: Char -> String -> Int
-countChar c str = length (filter (== c) str)
-
--- Cuenta cuántas veces una letra aparece marcada como LugarIncorrecto o Correcto
-countLugarIncorrectoCorrecto :: Char -> [(Char, Match)] -> Int
-countLugarIncorrectoCorrecto _ [] = 0
-countLugarIncorrectoCorrecto c ((char, m):xs) = 
-  if c == char && (m == LugarIncorrecto || m == Correcto)
-    then 1 + countLugarIncorrectoCorrecto c xs 
-    else countLugarIncorrectoCorrecto c xs
+-- Reemplaza el elemento en un índice específico en una lista
+reemplazarValor :: Int -> a -> [a] -> [a]
+reemplazarValor i x xs = take i xs ++ [x] ++ drop (i + 1) xs
